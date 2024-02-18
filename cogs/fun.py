bfe61ba567940aa9930da47fad206c048a5573c8
Nayla Hanegan <naylahanegan@gmail.com>
2024-02-18 15:11:14 -0500

    ugh darkspine

--- cogs/fun.py
+++ cogs/fun.py
@@ -62,7 +62,7 @@ class Fun(commands.Cog):
     @commands.slash_command(aliases=["random"])
     async def random_player (self, ctx):
         """Spits out 1, 2, or 3"""
-        awit ctx.respond(random.randint(1, 3))
+        await ctx.respond(random.randint(1, 3))
 
     #Dadjoke Command
     @commands.slash_command(aliases=["dadjoke"])
